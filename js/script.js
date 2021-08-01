93b0769c1308ece6331220bd0f030041d955dfc5
ndrety <88262884+ndrety@users.noreply.github.com>
2021-08-01 22:19:13 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,27 @@
+$(window).scroll (function(){
+		
+	var wScroll = $(this).scrollTop();
+	
+
+	
+	if ( wScroll > $('.contact').offset().top - 250){
+		$('.contact .contact-text').each(function(i){
+			setTimeout(function(){
+				$('.contact .contact-text').eq(i).addClass('muncul');
+			}, 200 * (i+1));
+		});
+	};
+	
+    
+	if ( wScroll > $('.panel').offset().top -350){
+		$('.panel .panel-item').each(function(i){
+			setTimeout(function(){
+				$('.panel .panel-item').eq(i).addClass('muncul');
+			}, 200 * (i+1));
+		});
+	};
+	
+
+	
+});
+
